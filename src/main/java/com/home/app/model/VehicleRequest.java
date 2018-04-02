ba8fba445325d33ac5341b6d8d09994dc0e84b01
ajoshi <ajoshi@xavient.com>
2018-04-02 20:47:21 +0530

    vehicle service

---
+++ src/main/java/com/home/app/model/VehicleRequest.java
@@ -0,0 +1,7 @@
+package com.home.app.model;
+
+public class VehicleRequest {
+	String vehicleType;
+	String vehicleCompany;
+	String vehicleNumber;
+}
---
+++ src/main/java/com/home/app/service/VehicleService.java
@@ -0,0 +1,12 @@
+package com.home.app.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VehicleService {
+	
+	public void save(){
+		
+	}
+
+}
